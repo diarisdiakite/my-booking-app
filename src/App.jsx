36e396d26c1901367038a475fefd112b3fe75d8f
thasipablo <thasipablo@gmail.com>
2023-12-11 00:55:13 +0100

    install react router dom, redux toolkit, tailwindcss, and add all linter files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="font-bold text-5xl text-slate-900">
+        <span className="text-pink-900">Welcome to</span>
+        <span>Luxury Car Booking</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
