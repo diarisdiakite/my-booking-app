f3a4a5e3f277cf3887fc91b972b300358e834e7c
diarisdiakite <diarisdiakite@gmail.com>
2023-12-16 19:52:31 +0100

    Update index.js and App.jsx files. Add store.js file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import Models from './pages/Models';
+import NavBar from './components/NavBar';
+import Car from './features/cars/Car';
+import CarsList from './features/cars/CarsList';
+import NotFoundPage from './components/NotFoundPage';
+// import UsersList from './features/users/UsersList';
+// import User from './features/users/User';
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/models" element={<Layout />}>
-        <Route index element={<Models />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-);
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/models" element={<Layout />}>
+          <Route index element={<Models />} />
+        </Route>
+        {' '}
+        <Route path="/cars">
+          <Route index element={<CarsList />} />
+          <Route path=":carId" element={<Car />} />
+        </Route>
+        {/* <Route path="/users">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<User />} />
+        </Route> */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './features/store';
+import './index.css';
+import App from './App';
+import {
+  fetchCars, setFetchedCars,
+} from './features/cars/carsSlice';
+// import 'bootstrap/dist/css/bootstrap.css';
+
+store.dispatch(fetchCars());
+store.dispatch(setFetchedCars());
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+);
