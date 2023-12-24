41b13446eb3dcaa12921c64d6dff57b07daec45b
thasipablo <thasipablo@gmail.com>
2023-12-24 05:14:20 +0100

    design home page

--- src/components/SideNavbar.jsx
+++ src/components/SideNavbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Twitter } from 'lucide-react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Logo from './Logo';
 
 const SideNavbar = () => (
-  <div className="w-[15%] py-3 border-r-2 border-slate-200">
+  <div className="w-[200px] py-3 border-r-2 border-slate-200">
     <div className="h-full flex flex-col justify-between">
       <div className="flex flex-col gap-14">
-        <Logo />
+        <Link to="/">
+          <Logo />
+        </Link>
         <div>
           <NavLink to="/dashboard/models" className="block p-3 uppercase">Models</NavLink>
           <NavLink to="/dashboard/lifestyle" className="block p-3 uppercase">Lifestyle</NavLink>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
+import { Search, Settings, ChevronRightCircle } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import homeImageBg from '../assets/images/bg.jpg';
 
 const Home = () => (
-  <div className="w-full h-screen flex justify-center items-center">
-    <div>
-      <h1 className="font-bold text-5xl text-slate-900">
-        <span className="text-pink-900"> Welcome to </span>
-        <span> Luxury Car Booking </span>
-      </h1>
-      <Link to="/dashboard/models" className="btn">
-        All Models
-      </Link>
+  <div className="relative w-full h-screen bg-orange-500/80">
+    <img className="w-fit opacity-10 top-0 bottom-0 left-0 right-0 absolute z-4" src={homeImageBg} alt="home background" />
+    <div className="absolute top-0 left-0 right-0">
+      <div className="flex justify-between p-6">
+        <div className="flex flex-col gap-2">
+          <div className="w-8 h-2 rounded-full bg-white" />
+          <div className="w-8 h-2 rounded-full bg-white" />
+        </div>
+        <div className="bg-white rounded-full py-2 px-4 text-orange-500">
+          <Search />
+        </div>
+      </div>
+    </div>
+    <div className="relative z-999">
+      <div className="w-full h-screen flex flex-col justify-center items-center -mt-6">
+        <h1 className="font-bold text-7xl text-white">
+          Luxury Car Booking
+        </h1>
+        <Link to="/dashboard/models" className="btn mt-4">
+          <div className="flex items-center gap-2 p-2">
+            <Settings className="w-5 h-5" />
+            <span>All Models</span>
+            <ChevronRightCircle className="w-5 h-5" />
+          </div>
+        </Link>
+      </div>
     </div>
   </div>
 );
